af5afd5ba09c04c7d9c8dc12e0a73f7ff928a8db
Claudia Melo <claudiamelocg@gmail.com>
2019-07-12 16:31:39 -0300

    Adicionando Arquivos

--- src/curso/Media1.java
+++ src/curso/Media1.java
@@ -1,9 +1,23 @@
 package curso;
-
+import java.util.Scanner;
 public class Media1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		
+		double numeroUm = teclado.nextDouble();
+		double numeroDois = teclado.nextDouble();
+		double peso1 = 3.5;
+		double peso2 = 7.5;
+		double var1 = (numeroUm*peso1) + (numeroDois*peso2);
+		double var2 = (peso1 + peso2);
+		double media = (var1) / (var2);
+		
+		
+		System.out.printf("MEDIA  = %.2f %n", + media);
+		
+		teclado.close();
+		
 
 	}
 
